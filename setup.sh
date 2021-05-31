ef4e639bdf0de27bc1f9f5f7093b6ba69e9aaf03
Giangonza <33987733+Giangonza@users.noreply.github.com>
2021-05-31 14:38:49 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
+# Setup.sh
 mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"gian.f.gonza@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+email = \"your-email@domain.com\"\n\
+" >~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
+" >~/.streamlit/config.toml
